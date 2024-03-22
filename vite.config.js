772490c8a0d0c832269565fbb8fa5bbb996cf152
HomeArchbishop <1984096954@qq.com>
2024-03-22 18:52:56 +0800

    chores: update vite path alias

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve('src'),
-      comps: path.resolve('src/components'),
-      apis: path.resolve('src/apis'),
-      views: path.resolve('src/views'),
-      utils: path.resolve('src/utils'),
-      routes: path.resolve('src/routes'),
-      styles: path.resolve('src/styles')
+      '@': path.resolve('src')
     }
   }
 })
