fa830601498307d64ed38c9a3292cd1c6cc65211
HomeArchbishop <1984096954@qq.com>
2024-03-25 13:26:33 +0800

    fix: use lazy load on routers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,8 @@
-// FIXME: lazy load of routes may cause stuck in hot-dev
-
-import HomeView from '@/views/HomeView.vue'
-
 const routes = [
   {
     path: '/',
     name: 'HomeView',
-    component: HomeView, // () => import('@/views/HomeView.vue'),
+    component: () => import('@/views/HomeView.vue'),
     meta: {
       keepAlive: true,
       savePosition: true
